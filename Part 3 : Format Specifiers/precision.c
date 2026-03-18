6d4b6b1e69aa3529c8434c89bcf7cb948df0918c
byt23 <berkayyasar2004@gmail.com>
2026-03-18 22:13:10 +0300

    Matematik işlemlerinede çalıştım

---
+++ Part 3 : Format Specifiers/precision.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+
+    float price1 = 15.99;
+    float price2 = 1.50;
+    float price3 = -100.00;
+
+    printf("%.2f\n",price1);
+    printf("%.2f\n",price2);
+    printf("%.2f\n",price3);
+
+
+    printf("%+7.2f\n",price1);
+    printf("%+7.2f\n",price2);
+    printf("%+7.2f\n",price3);
+
+    return 0;
+}
---
+++ Part 4 : Arithmetic Operators/plus.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+
+    int x = 2;
+    int y = 3;
+    int z = 0;
+
+    x += 2;
+
+    printf("%d",x);
+
+    return 0;
+}
