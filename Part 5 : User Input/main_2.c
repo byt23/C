7bfc75aadf9c41bd1b6594634e37fe281cc4af72
byt23 <berkayyasar2004@gmail.com>
2026-03-19 00:47:23 +0300

    Pratik yaptım.

---
+++ Part 5 : User Input/main_2.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+
+    int age = 0;
+    float gpa = 0.0f;
+    char grade = '\0';
+    char name[50] = "";
+
+    printf("Enter your age : ");
+    scanf("%d",&age);
+
+    printf("Enter your gpa : ");
+    scanf("%f",&gpa);
+
+    printf("Enter your grade : ");
+    scanf(" %c",&grade);
+
+    getchar();
+    printf("Enter your name : ");
+    fgets(name, sizeof(name), stdin);
+    name[strlen(name) - 1] = '\0';
+
+    printf("You are %d old.\n", age);
+    printf("Your gpa is %.2f.\n", gpa);
+    printf("Your grade is %c.\n", grade);
+    printf("Your name is %s.\n", name);
+
+    return 0;
+}
