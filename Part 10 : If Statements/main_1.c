e539e07eff009c21732bb9fd0742a42ac0d4ef00
byt23 <berkayyasar2004@gmail.com>
2026-03-21 00:29:50 +0300

    If else alıştırması yaptım.

---
+++ Part 10 : If Statements/main_1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    int age = 0;
+
+    printf("Enter your age : ");
+    scanf("%d",&age);
+    
+    if(age >= 65){
+        printf("You are a senior");
+    }
+    else if(age >= 18){
+        printf("You are an adult");
+    }
+    else if(age < 0) {
+        printf("You haven't born yet");
+    }
+
+    else if(age == 0) {
+        printf("You are a newborn");
+    }
+
+    else {
+        printf("You are a child");
+    }
+
+
+    return 0;
+}
---
+++ Part 10 : If Statements/main_2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main(){
+
+    bool isStudent = true;
+    
+    if(isStudent == true) {
+        printf("You are a student");
+    }
+    else {
+        printf("You are NOT a student");
+    }
+
+    return 0;
+
+}
